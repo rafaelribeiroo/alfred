afdc194b2aa599c6ff3cb94821a4fd29ba30b481
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2020-03-29 05:02:21 -0300

    Including tests, it's for testing new issues

---
+++ tests.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+: 'fdisk -l  # Cheque qual o disco primário (primeiro)
+
+# dd: alocar uma parte do seu disco gravando-o em um arquivo.
+# if: vou extrair um espaço em disco de...
+# of: colocar ele...
+# bs: tamanho dele
+# count: vou pegar apenas uma vez o espaço informado no bs
+dd if=/dev/sda of=/swapfile bs=2G count=1  # Conceder swap
+mkswap /swapfile  # formatar
+swapon /swapfile  # ativar
+
+Ao reiniciar, o swapfile vai ser perdido, a não ser que:
+
+/etc/fstab
+/swapfile swap swap defaults 0 0'
+
+# systemctl list-unit-files --type service -all
+# escreva "${c[VERMELHO]}=======================================================" 1
+
+if [[ 4 -eq 3 ]]; then
+
+    echo ":)"
+
+else
+
+    [[ ! $(apt-key list 2> /dev/null | grep "postgre") ]] \
+        && echo exists || echo not exists
+
+fi
+
+
+: ' aumenta_valor() {
+    if [[ ${1} -eq 1 ]]; then
+        return 0
+    elif [[ ${1} -eq 2 ]]; then
+        return 1
+    fi
+}
+
+aumenta_valor 1
+if [[ "$?" -eq 0 ]]; then
+    echo "Vim do if do aumenta_valor"
+fi'
+
+# ( nohup firefox & ) > "${f[35]}" 2>&1
+
+#Quando instalamos o linux, o driver nouveau vem instalado por default, open-source, eles nao conseguem extrair o maximo do hardware
+#ngm conhece melhor que nvidia que é quem faz o hardware
+
+#entao precisamos colocar o nouveau na blacklist
