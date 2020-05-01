2b65894c96627646fada32d81d3cdf17c5f1d3da
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2020-04-30 21:53:37 -0300

    testing quotes alignment

--- tests.sh
+++ tests.sh
@@ -9,6 +9,31 @@ declare -A c=(
     [YELLOW]='\033[1;33m'
 )
 
+e=(
+    $'\360\237\232\252'  #  0 (door): exit
+    $'\360\237\216\250'  #  1 (colors): bash colorful
+    $'\360\237\216\247'  #  2 (headphone): deezloader
+    $'\360\237\214\211'  #  3 (landscape): dualmonitor
+    $'\360\237\220\231'  #  4 (octopus): git
+    $'\360\237\214\215'  #  5 (globe): chrome
+    $'\360\237\223\267'  #  6 (camera): flameshot
+    $'\360\237\232\200'  #  7 (rocket): heroku
+    $'\360\237\231\210'  #  8 (blindfolded monkey): hide devices
+    $'\360\237\215\277'  #  9 (popcorn): minidlna
+    $'\360\235\223\235'  # 10 (n): nvidia
+    $'\360\237\220\230'  # 11 (elephant): postgres
+    $'\360\237\220\215'  # 12 (snake): py libraries/upgrade
+    $'\360\237\214\230'  # 13 (moon): reduce eye strain
+    $'\360\237\224\244'  # 14 (letters): sublime
+    $'\360\237\247\262'  # 15 (magnet): tmate
+    $'\360\237\222\216'  # 16 (diamond): usefull programs
+    $'\360\237\222\274'  # 17 (suitcase): workspace
+    $'\360\237\220\213'  # 18 (whale): all
+    $'\360\237\224\245'  # 19 (fire): some men...
+    $'\360\237\231\212'  # 20 (silent monkey): password
+    $'\360\237\246\207'  # 21 (bat): why do we fall...
+)
+
 show() {
 
     echo -e ${c[WHITE]}"${1}"${c[END]}
@@ -40,6 +65,6 @@ declare -A f=(
     [file]=~/.pyenv/shims/python
 )
 
-# show "${c[RED]}=======================================================" 1
+show "${c[RED]}=======================================================" 1
 
-# git ls-remote git@github.com:rafaelribeiroo/catalina &> /tmp/check_repo
+show "\n\t\t${c[RED]}REPO ALREADY DOWNLOADED"
